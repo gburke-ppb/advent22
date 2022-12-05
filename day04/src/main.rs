9827f2c7ebc1e10c5c529139550a8a225eb47cb5
Gary Burke <gary.burke@paddypowerbetfair.com>
2022-12-05 13:17:11 +0000

    part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -13,6 +13,7 @@ pub fn main() {
 }
 
 // outer and inner are of the form number-number (e.g. 4-19)
+/** does outer contain inner? */
 fn contains(outer: &str, inner: &str) -> bool {
   let outer_str = String::from(outer);
   let mut outer_range = outer_str.split("-");
@@ -42,14 +43,41 @@ fn part1(filename: &String) {
       }
     }
   }
-  println!("Part one: Total score = {count}");
+  println!("Part one: answer = {count}");
+}
+
+/** does first overlap with second? */
+fn overlap(first: &str, second: &str) -> bool {
+  let first_str = String::from(first);
+  let mut first_range = first_str.split("-");
+  let first_lower = first_range.next().unwrap().parse::<u32>().unwrap();
+  let first_upper = first_range.next().unwrap().parse::<u32>().unwrap();
+
+  let second_str = String::from(second);
+  let mut second_range = second_str.split("-");
+  let second_lower = second_range.next().unwrap().parse::<u32>().unwrap();
+  let second_upper = second_range.next().unwrap().parse::<u32>().unwrap();
+
+  (first_lower >= second_lower && first_lower <= second_upper)
+    || (first_upper >= second_lower && first_upper <= second_upper)
+    || (second_lower >= first_lower && second_lower <= first_upper)
+    || (second_upper >= first_lower && second_upper <= first_upper)
 }
 
 fn part2(filename: &String) {
   let infile = BufReader::new(File::open(filename).expect("Can't open that file"));
 
+  let mut count: u32 = 0;
+
   for input in infile.lines() {
-    if let Ok(line) = input {}
+    if let Ok(line) = input {
+      let mut assignments = line.split(",");
+      let elf_a = assignments.next().unwrap();
+      let elf_b = assignments.next().unwrap();
+      if overlap(elf_a, elf_b) || contains(elf_b, elf_a) {
+        count += 1;
+      }
+    }
   }
-  println!("Part two: Total score = {}", 0);
+  println!("Part two: answer = {count}");
 }
