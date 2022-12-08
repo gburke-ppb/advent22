3f811a0d92d668fe3aa3f5fb6d7cd9a2c24abc5d
Gary Burke <gary.burke@paddypowerbetfair.com>
2022-12-08 15:43:24 +0000

    parts 1 and 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,105 @@
-fn main() {
-  println!("Hello, world!");
+use std::{
+  env,
+  fs::File,
+  io::{BufRead, BufReader},
+};
+
+enum InodeType {
+  Dir,
+  File,
+}
+
+struct Inode {
+  name: String,
+  itype: InodeType, // `type` is a reserved word
+  size: u64,
+  children: Vec<Inode>,
+}
+
+impl Inode {
+  fn add_dir(&mut self, dirname: &String) {
+    let node = Inode {
+      name: String::from(dirname),
+      itype: InodeType::Dir,
+      size: 0,
+      children: Vec::new(),
+    };
+    self.children.push(node);
+  }
+
+  fn add_file(&mut self, filename: &str, size: u64) {
+    let node = Inode {
+      name: String::from(filename),
+      itype: InodeType::File,
+      size: size,
+      children: Vec::new(),
+    };
+    self.children.push(node);
+    self.size += size;
+  }
+}
+
+pub fn main() {
+  let args: Vec<String> = env::args().collect();
+  let filename = &args[1];
+
+  part1(&filename);
+  part2(&filename);
+}
+
+fn part1(filename: &String) {
+  let infile = BufReader::new(File::open(filename).expect("Can't open that file"));
+  let mut lines = infile.lines();
+
+  let mut fs: Inode = Inode {
+    name: String::from("/"),
+    itype: InodeType::Dir,
+    size: 0, // if a file, the size of the file.  If a dir, the size of all files and subdirs
+    children: Vec::new(),
+  };
+
+  while let Some(input) = lines.next() {
+    if let Ok(mut line) = input {
+      if line.starts_with("$ ") {
+        let mut command = line.split_off(2);
+        if command.starts_with("cd ") {
+          let dirname = command.split_off(3);
+          if dirname == ".." {
+
+          } else if dirname == "/" {
+
+          } else {
+
+          }
+        } else if command.starts_with("ls") {
+          continue;
+        }
+
+      } else if line.starts_with("dir") {
+        //
+        let dirname = line.split_off(4);
+        // something.add_dir(&dirname);
+      } else {
+        // it starts with a number.
+        let strings: Vec<_> = line.split(" ").collect();
+        let str_size = strings[0];
+        let filename = strings[1];
+        let size: usize = str_size.parse().unwrap();
+
+        // something.add_file(&filename, size);
+      }
+    }
+  }
+
+  print!("Part one: answer = ");
+}
+
+fn part2(filename: &String) {
+  let infile = BufReader::new(File::open(filename).expect("Can't open that file"));
+  let mut lines = infile.lines();
+  while let Some(input) = lines.next() {
+    if let Ok(line) = input {}
+  }
+
+  print!("Part two: answer = ");
 }
--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,3 +1,109 @@
-fn main() {
-  println!("Hello, world!");
+use std::{
+  env,
+  fs::File,
+  io::{BufRead, BufReader},
+};
+
+pub fn main() {
+  let args: Vec<String> = env::args().collect();
+  let filename = &args[1];
+
+  both_parts(&filename);
+}
+
+fn check_visiblity(forest: &Vec<Vec<u32>>, x: usize, y: usize) -> (bool, usize) {
+  let height = forest.len();
+  let width = forest[0].len();
+  let val = forest[x][y];
+  let mut result: [bool; 4] = [true; 4];
+  let mut dist: [usize; 4] = [1; 4];
+
+  let mut count: usize = 0;
+  for i in (0..x).rev() {
+    count += 1;
+    if val <= forest[i][y] {
+      result[0] = false;
+      break;
+    }
+  }
+  dist[0] = count;
+
+  count = 0;
+  for i in x + 1..width {
+    count += 1;
+    if val <= forest[i][y] {
+      result[1] = false;
+      break;
+    }
+  }
+  dist[1] = count;
+
+  count = 0;
+  for i in (0..y).rev() {
+    count += 1;
+    if val <= forest[x][i] {
+      result[2] = false;
+      break;
+    }
+  }
+  dist[2] = count;
+
+  count = 0;
+  for i in y + 1..height {
+    count += 1;
+    if val <= forest[x][i] {
+      result[3] = false;
+      break;
+    }
+  }
+  dist[3] = count;
+
+  return (
+    result[0] | result[1] | result[2] | result[3],
+    dist[0] * dist[1] * dist[2] * dist[3],
+  );
+}
+
+fn count_visible_trees(forest: &Vec<Vec<u32>>) -> (usize, usize) {
+  let mut count: usize = 0;
+  let height = forest.len();
+  let width = forest[0].len();
+  count += (height * 2) + (width * 2) - 4; // the corners are doubled
+
+  let mut max_scenic_score: usize = 1;
+  for x in 1..width - 1 {
+    for y in 1..height - 1 {
+      let (visible, scenic_score) = check_visiblity(&forest, x, y);
+      if visible {
+        count += 1
+      }
+      if scenic_score > max_scenic_score {
+        max_scenic_score = scenic_score;
+      }
+    }
+  }
+
+  (count, max_scenic_score)
+}
+
+fn both_parts(filename: &String) {
+  let infile = BufReader::new(File::open(filename).expect("Can't open that file"));
+  let mut lines = infile.lines();
+
+  let mut forest: Vec<Vec<u32>> = Vec::new();
+
+  while let Some(input) = lines.next() {
+    if let Ok(line) = input {
+      let mut row: Vec<u32> = Vec::new();
+      for c in line.chars() {
+        row.push(c.to_digit(10).unwrap());
+      }
+      forest.push(row);
+    }
+  }
+
+  let (count, max_scenic_score) = count_visible_trees(&forest);
+
+  println!("Part one: answer = {count}");
+  println!("Part two: answer = {max_scenic_score}");
 }
